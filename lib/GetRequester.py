1873530ad1925b7f502ab3d7f48f3ed13de87ae7
Aidin <aidin.forooghi@gmail.com>
2023-09-02 09:27:29 -0400

    Passed Tests

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,16 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        if response_body:
+            try:
+                decoded_content = response_body.decode('utf-8')
+                return json.loads(decoded_content)
+            except json.JSONDecodeError:
+                return None
+        else:
+            return None
